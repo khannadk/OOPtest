02992cf28e04128c3e05b949766c27e7c8ae1491
Dev Kha <devkhanna@outlook.com>
2021-02-27 12:24:00 -0500

    app.js update

--- develop/app.js
+++ develop/app.js
@@ -10,7 +10,225 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const team = [];
 
+function renderSite(){
+if(!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR)
+}fs.writeFileSync(outputPath, render(team), "UTF8")
+}
+
+function switchStatement() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Which Employee",
+        choices: ["Engineer", "Intern", "Manager", "Done"],
+        name: "switch",
+      },
+    ])
+    .then(function (answer) {
+      switch (answer.switch) {
+        case "Engineer":
+          askEngineer();
+          break;
+        case "Intern":
+          askIntern();
+          break;
+        case "Manager":
+          askManager();
+          break;
+
+        case "Done":
+          renderSite();
+          break;
+      }
+    });
+}
+
+const managerQuestions = [
+  {
+    type: "input",
+    name: "managerName",
+    message: "What is your name",
+    validate: function(answer) {
+      if(answer !== "") {
+          return true
+      } return "please enter your name"
+  }
+  },
+  {
+    type: "input",
+    name: "managerId",
+    message: "What is your ID",
+    validate: function(answer){
+      if(!isNaN(answer)){
+          return true
+      }return "please enter a number"
+  }
+  },
+  {
+    type: "input",
+    name: "managerOffice",
+    message: "What is your office number",
+    validate: function(answer){
+      if(!isNaN(answer)){
+          return true
+      }return "please enter a number"
+  }
+  },
+  {
+    type: "input",
+    name: "managerEmail",
+    message: "What is your E-mail",
+    validate: function(answer) {
+      if(answer !== "") {
+          return true
+      } return "please enter your E-mail"
+  }
+  },
+];
+const engineerQuestions = [
+  {
+    type: "input",
+    name: "engineerName",
+    message: "What is your name",
+    validate: function(answer) {
+      if(answer !== "") {
+          return true
+      } return "please enter your name"
+  }
+  },
+  {
+    type: "input",
+    name: "engineerId",
+    message: "What is your ID",
+    validate: function(answer){
+      if(!isNaN(answer)){
+          return true
+      }return "please enter a number"
+  }
+  },
+  {
+    type: "input",
+    name: "engineerGithub",
+    message: "What is your GitHub",
+    validate: function(answer) {
+      if(answer !== "") {
+          return true
+      } return "please enter your GibHub name"
+  }
+  },
+  {
+    type: "input",
+    name: "engineerEmail",
+    message: "What is your E-mail",
+    validate: function(answer) {
+      if(answer !== "") {
+          return true
+      } return "please enter your E-mail"
+  }
+  },
+];
+
+const internQuestions = [
+  {
+    type: "input",
+    name: "internName",
+    message: "What is your name",
+    validate: function(answer) {
+        if(answer !== "") {
+            return true
+        } return "please enter your name"
+    }
+  },
+  {
+    type: "input",
+    name: "internId",
+    message: "What is your ID",
+    validate: function(answer){
+        if(!isNaN(answer)){
+            return true
+        }return "please enter a number"
+    }
+  },
+  {
+    type: "input",
+    name: "internSchool",
+    message: "What is your school",
+    validate: function(answer) {
+      if(answer !== "") {
+          return true
+      } return "please enter your school"
+  }
+  },
+  {
+    type: "input",
+    name: "internEmail",
+    message: "What is your E-mail",
+    validate: function(answer) {
+      if(answer !== "") {
+          return true
+      } return "please enter your E-Mail"
+  }
+  },
+];
+
+function askIntern() {
+  inquirer
+    .prompt(internQuestions)
+    .then(function ({ internName, internEmail, internId, internSchool }) {
+      const intern = new Intern(
+        internName,
+        internId,
+        internEmail,
+        internSchool
+      );
+      console.log(intern);
+      team.push(intern);
+      switchStatement();
+    });
+}
+
+function askManager() {
+  inquirer
+    .prompt(managerQuestions)
+    .then(function ({ managerName, managerEmail, managerId, managerOffice }) {
+      const manager = new Manager(
+        managerName,
+        managerId,
+        managerEmail,
+        managerOffice
+      );
+      console.log(manager);
+      team.push(manager);
+      switchStatement();
+    });
+}
+
+function askEngineer() {
+  inquirer
+    .prompt(engineerQuestions)
+    .then(function ({
+      engineerName,
+      engineerEmail,
+      engineerId,
+      engineerGithub,
+    }) {
+      const engineer = new Engineer(
+        engineerName,
+        engineerId,
+        engineerEmail,
+        engineerGithub
+      );
+      console.log(engineer);
+      team.push(engineer);
+      switchStatement();
+    });
+}
+
+switchStatement();
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
@@ -33,3 +251,4 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
